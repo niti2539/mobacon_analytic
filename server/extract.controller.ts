d2a3907100a2e471eaad85f09b1bfe8c2adef611
Nitipan Pompan <nitipan.p@hotmail.com>
2018-10-24 22:38:35 +0700

    added more extraction logic

--- server/extract.controller.ts
+++ server/extract.controller.ts
@@ -65,6 +65,83 @@ router.post('/softbank/:correlationid', (req: any, res) => {
     extract.communicationFee = items;
 
 
+    // communication fee
+    const optionContent = findString(html, '<em id="option-services">オプションサービス料', '</table');
+    items = [];
+    item = {};
+    optionContent.split(/\n/).forEach(line => {
+        const key = /<td>(.*?)<\/td>/.exec(line);
+        if (key)
+            item.key = key[1].trim();
+        else {
+            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+            if (value) {
+                item.value = value[2].trim();
+                items.push(item);
+                item = {};
+            } else {
+                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
+                if (value) {
+                    item.value = value[2].trim();
+                    items.push(item);
+                    item = {};
+                }
+            }
+        }
+    });
+    extract.optionServiceCharge = items;
+
+    // communication fee
+    const universalContent = findString(html, '<em>ユニバーサルサービス料</em>', '</table');
+    items = [];
+    item = {};
+    universalContent.split(/\n/).forEach(line => {
+        const key = /<td>(.*?)<\/td>/.exec(line);
+        if (key)
+            item.key = key[1].trim();
+        else {
+            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+            if (value) {
+                item.value = value[2].trim();
+                items.push(item);
+                item = {};
+            } else {
+                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
+                if (value) {
+                    item.value = value[2].trim();
+                    items.push(item);
+                    item = {};
+                }
+            }
+        }
+    });
+    extract.universalServiceFee = items;
+
+    // discount
+    const discountContent = findString(html, '<em>割引</em>', '</table');
+    items = [];
+    item = {};
+    discountContent.split(/\n/).forEach(line => {
+        const key = /<td>(.*?)<\/td>/.exec(line);
+        if (key)
+            item.key = key[1].trim();
+        else {
+            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+            if (value) {
+                item.value = value[2].trim();
+                items.push(item);
+                item = {};
+            } else {
+                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
+                if (value) {
+                    item.value = value[2].trim();
+                    items.push(item);
+                    item = {};
+                }
+            }
+        }
+    });
+    extract.discount = items;
 
     res.json(extract);
 });
