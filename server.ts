f744d56d9bca4d464ef794f19ff651f5c87b0c63
Nitipan Pompan <nitipan.p@hotmail.com>
2018-11-12 21:40:32 +0700

    added db maagement

--- server.ts
+++ server.ts
@@ -5,43 +5,45 @@ import bodyParser from 'body-parser';
 
 import { ExtractController } from './server/extract.controller';
 import { BillingsController } from './server/billings.controller';
+import { createConnection } from 'typeorm';
 
+createConnection().then(async connection => {
 
-const app: express.Application = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+    const app: express.Application = express();
+    const http = require('http').Server(app);
+    const io = require('socket.io')(http);
 
-app.set('socketio', io);
+    app.set('socketio', io);
 
-io.sockets.on("connection", (socket: any) => {
-    // Display a connected message
-    console.log("Server-Client Connected!");
-
-
-});
-
-const port: any = process.env.PORT || 3000;
-
-function xmlParser(req: any, res: any, next: any) {
-    let data = '';
-    req.setEncoding('utf8');
-    req.on('data', function (chunk: any) {
-        data += chunk;
+    io.sockets.on("connection", (socket: any) => {
+        // Display a connected message
+        console.log("Server-Client Connected!");
     });
-    req.on('end', function () {
-        req.rawBody = data;
-        next();
+
+    const port: any = process.env.PORT || 3000;
+
+    function xmlParser(req: any, res: any, next: any) {
+        let data = '';
+        req.setEncoding('utf8');
+        req.on('data', function (chunk: any) {
+            data += chunk;
+        });
+        req.on('end', function () {
+            req.rawBody = data;
+            next();
+        });
+    }
+
+    app.use(xmlParser);
+
+    app.use(bodyParser.json());
+
+    app.use('/api/extract', ExtractController);
+    app.use('/api/billings', BillingsController);
+
+
+    http.listen(port, () => {
+        console.log(`Listening at http://localhost:${port}/`);
     });
-}
 
-app.use(xmlParser);
-
-app.use(bodyParser.json());
-
-app.use('/api/extract', ExtractController);
-app.use('/api/billings', BillingsController);
-
-
-http.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/`);
-});
+}).catch(error => console.log("TypeORM connection error: ", error));
