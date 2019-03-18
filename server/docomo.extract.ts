762de7cbffa3d9a5f6a5601d8cb8e7502eeff7bd
NITIPANP <NITIPANP@THESL00592>
2019-03-18 18:38:24 +0700

    added docomo exraction

---
+++ server/docomo.extract.ts
@@ -0,0 +1,82 @@
+import { findString } from "./string-helper";
+import { Billing } from "../shared/billing";
+import { getManager } from "typeorm";
+
+const docomo = async (req: any, res) => {
+  const correlationid = req.params.correlationid;
+
+  const html: string = req.rawBody;
+
+  const extract: any = {};
+
+  let items: any = [];
+  let item: any = {};
+  let block;
+  block = {};
+
+  block.items = [];
+
+  html.split(/\n/).forEach(line => {
+    line = line.trim();
+    const title = /<span class="card-ico-charge(.*?)">(.*?)<\/span>/.exec(line);
+    if (title) {
+      block.date = title[2].trim();
+      return;
+    }
+  });
+
+  const paymentDetail = findString(
+    html,
+    '<div id="mydcm_payment_detail-02">',
+    "</body>"
+  );
+
+  paymentDetail.split(/\n/).forEach(line => {
+    line = line.trim();
+
+    if (true) {
+      const key = /<(span|dt) class="mydcm_payment_detail-name(.*?)">(.*?)<\/(span|dt)>/.exec(
+        line
+      );
+      if (key) {
+        item.key = key[3].trim();
+      }
+
+      if (item.key) {
+        let value = /<(span|dd) class="mydcm_payment_detail_amount(.*?)">(.*?)<\/(span|dd)>/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[3].trim();
+
+          block.items.push(item);
+          item = {};
+        }
+      }
+    }
+  });
+
+  extract.basicUsage = block;
+
+  const billingRepo = getManager().getRepository(Billing);
+
+  let billing: any = {
+    userId: correlationid,
+    createdDate: new Date(),
+    data: extract
+  };
+
+  await billingRepo.save(billing);
+
+  let message: any = {
+    userId: correlationid,
+    billingUrl: `/api/billings/${billing.id}`,
+    createdDate: billing.createdDate
+  };
+
+  const io = req.app.get("socketio");
+  io.emit("billing.new", message);
+  res.json(message);
+};
+
+export { docomo };
--- server/extract.controller.ts
+++ server/extract.controller.ts
@@ -1,278 +1,271 @@
-
-import { Router, Request, Response } from 'express';
-import { findString } from './string-helper';
-import { Billing } from '../shared/billing';
-import { getManager } from 'typeorm';
-import { ymobile } from './ymobile.extract';
-import { au } from './au.extract';
-
+import { Router, Request, Response } from "express";
+import { findString } from "./string-helper";
+import { Billing } from "../shared/billing";
+import { getManager } from "typeorm";
+import { ymobile } from "./ymobile.extract";
+import { au } from "./au.extract";
+import { docomo } from "./docomo.extract";
 const router: Router = Router();
 
-router.post('/softbank/:correlationid', async (req: any, res) => {
+router.post("/softbank/:correlationid", async (req: any, res) => {
+  const correlationid = req.params.correlationid;
 
-    const correlationid = req.params.correlationid;
+  const html: string = req.rawBody;
 
-    const html: string = req.rawBody;
+  const extract: any = {};
 
-    const extract: any = {};
-
-    // basic usage
-    const buContent = findString(html, '<em>基本料', '</table');
-    let items: any = [];
-    let item: any = {};
-    buContent.split(/\n/).forEach(line => {
-        const key = /<td>(.*?)<\/td>/.exec(line);
-        if (key)
-            item.key = key[1].trim();
-        else {
-            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
-            if (value) {
-                item.value = value[2].trim();
-                items.push(item);
-                item = {};
-            } else {
-                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
-                if (value) {
-                    item.value = value[2].trim();
-                    items.push(item);
-                    item = {};
-                }
-            }
+  // basic usage
+  const buContent = findString(html, "<em>基本料", "</table");
+  let items: any = [];
+  let item: any = {};
+  buContent.split(/\n/).forEach(line => {
+    const key = /<td>(.*?)<\/td>/.exec(line);
+    if (key) item.key = key[1].trim();
+    else {
+      let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+      if (value) {
+        item.value = value[2].trim();
+        items.push(item);
+        item = {};
+      } else {
+        value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[2].trim();
+          items.push(item);
+          item = {};
         }
-    });
-    extract.basicUsage = items;
-
-    // communication fee
-    const comFeeContent = findString(html, '<em>通信料', '</table');
-    items = [];
-    item = {};
-    comFeeContent.split(/\n/).forEach(line => {
-        const key = /<td>(.*?)<\/td>/.exec(line);
-        if (key)
-            item.key = key[1].trim();
-        else {
-            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
-            if (value) {
-                item.value = value[2].trim();
-                items.push(item);
-                item = {};
-            } else {
-                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
-                if (value) {
-                    item.value = value[2].trim();
-                    items.push(item);
-                    item = {};
-                }
-            }
-        }
-    });
-    extract.communicationFee = items;
-
-
-    // communication fee
-    const optionContent = findString(html, '<em id="option-services">オプションサービス料', '</table');
-    items = [];
-    item = {};
-    optionContent.split(/\n/).forEach(line => {
-        const key = /<td>(.*?)<\/td>/.exec(line);
-        if (key)
-            item.key = key[1].trim();
-        else {
-            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
-            if (value) {
-                item.value = value[2].trim();
-                items.push(item);
-                item = {};
-            } else {
-                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
-                if (value) {
-                    item.value = value[2].trim();
-                    items.push(item);
-                    item = {};
-                }
-            }
-        }
-    });
-    extract.optionServiceCharge = items;
-
-    // communication fee
-    const universalContent = findString(html, '<em>ユニバーサルサービス料</em>', '</table');
-    items = [];
-    item = {};
-    universalContent.split(/\n/).forEach(line => {
-        const key = /<td>(.*?)<\/td>/.exec(line);
-        if (key)
-            item.key = key[1].trim();
-        else {
-            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
-            if (value) {
-                item.value = value[2].trim();
-                items.push(item);
-                item = {};
-            } else {
-                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
-                if (value) {
-                    item.value = value[2].trim();
-                    items.push(item);
-                    item = {};
-                }
-            }
-        }
-    });
-    extract.universalServiceFee = items;
-
-    // discount
-    const discountContent = findString(html, '<em>割引</em>', '</table');
-    items = [];
-    item = {};
-    discountContent.split(/\n/).forEach(line => {
-        const key = /<td>(.*?)<\/td>/.exec(line);
-        if (key)
-            item.key = key[1].trim();
-        else {
-            let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
-            if (value) {
-                item.value = value[2].trim();
-                items.push(item);
-                item = {};
-            } else {
-                value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(line);
-                if (value) {
-                    item.value = value[2].trim();
-                    items.push(item);
-                    item = {};
-                }
-            }
-        }
-    });
-    extract.discount = items;
-
-
-
-    const billingRepo = getManager().getRepository(Billing);
-
-    let billing: any = {
-        userId: correlationid,
-        createdDate: new Date(),
-        data: extract
-    };
-
-    await billingRepo.save(billing);
-
-    let message: any = {
-        userId: correlationid,
-        billingUrl: `/api/billings/${billing.id}`,
-        createdDate: billing.createdDate
-    };
-
-    const io = req.app.get('socketio');
-    io.emit('billing.new', message);
-    res.json(message);
-});
-
-router.post('/docomo/:correlationid', (req: any, res) => {
-
-    const correlationid = req.params.correlationid;
-
-    const html = req.rawBody;
-
-
-
-    res.json({ 'status': 'OK' });
-});
-
-router.post('/kddi/:correlationid', async (req: any, res) => {
-
-    const correlationid = req.params.correlationid;
-
-    const html: string = req.rawBody;
-
-    const extract: any = {};
-
-
-    // basic usage
-    const buContent = findString(html, '<h1>今月及び前月のデータ通信量</h1>', '<div class="leadSection');
-    let items: any = [];
-    let item: any;
-    let block: any;
-
-    buContent.split(/\n/).forEach(line => {
-
-        line = line.trim();
-        if (line.startsWith('<h4>')) {
-            if (block) {
-
-                if (item) {
-                    block.items.push(item);
-                    item = undefined;
-                }
-
-                items.push(block);
-            }
-
-            block = {};
-            block.date = findString(line, '<h4>', '</h4>');
-            block.items = [];
-        } else {
-            if (line.startsWith('<div class="cell alignR">')) {
-                let val = findString(line, '<div class="cell alignR">', '</div>');
-                if (item.value)
-                    val += '|' + val;
-                else
-                    item.value = '';
-
-                item.value += val;
-
-            } else {
-                let key = findString(line, '<div class="cell item">', '</div>');
-                if (key) {
-
-                    if (item) {
-                        block.items.push(item);
-                        item = undefined;
-                    }
-
-                    if (!item) {
-                        item = {
-                            key: key
-                        };
-                    }
-                }
-            }
-        }
-
-    });
-
-    if (item) {
-        block.items.push(item);
+      }
     }
+  });
+  extract.basicUsage = items;
 
-    items.push(block);
+  // communication fee
+  const comFeeContent = findString(html, "<em>通信料", "</table");
+  items = [];
+  item = {};
+  comFeeContent.split(/\n/).forEach(line => {
+    const key = /<td>(.*?)<\/td>/.exec(line);
+    if (key) item.key = key[1].trim();
+    else {
+      let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+      if (value) {
+        item.value = value[2].trim();
+        items.push(item);
+        item = {};
+      } else {
+        value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[2].trim();
+          items.push(item);
+          item = {};
+        }
+      }
+    }
+  });
+  extract.communicationFee = items;
 
-    extract.basicUsage = items;
+  // communication fee
+  const optionContent = findString(
+    html,
+    '<em id="option-services">オプションサービス料',
+    "</table"
+  );
+  items = [];
+  item = {};
+  optionContent.split(/\n/).forEach(line => {
+    const key = /<td>(.*?)<\/td>/.exec(line);
+    if (key) item.key = key[1].trim();
+    else {
+      let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+      if (value) {
+        item.value = value[2].trim();
+        items.push(item);
+        item = {};
+      } else {
+        value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[2].trim();
+          items.push(item);
+          item = {};
+        }
+      }
+    }
+  });
+  extract.optionServiceCharge = items;
 
-    const billingRepo = getManager().getRepository(Billing);
+  // communication fee
+  const universalContent = findString(
+    html,
+    "<em>ユニバーサルサービス料</em>",
+    "</table"
+  );
+  items = [];
+  item = {};
+  universalContent.split(/\n/).forEach(line => {
+    const key = /<td>(.*?)<\/td>/.exec(line);
+    if (key) item.key = key[1].trim();
+    else {
+      let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+      if (value) {
+        item.value = value[2].trim();
+        items.push(item);
+        item = {};
+      } else {
+        value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[2].trim();
+          items.push(item);
+          item = {};
+        }
+      }
+    }
+  });
+  extract.universalServiceFee = items;
 
-    let billing: any = {
-        userId: correlationid,
-        createdDate: new Date(),
-        data: extract
-    };
+  // discount
+  const discountContent = findString(html, "<em>割引</em>", "</table");
+  items = [];
+  item = {};
+  discountContent.split(/\n/).forEach(line => {
+    const key = /<td>(.*?)<\/td>/.exec(line);
+    if (key) item.key = key[1].trim();
+    else {
+      let value = /<td(.*?)><span>(.*?)<\/span>/.exec(line);
+      if (value) {
+        item.value = value[2].trim();
+        items.push(item);
+        item = {};
+      } else {
+        value = /<td(.*?)>([+-]?\$?[1-9]\d?(?:,*\d{3})*(?:\.\d{2})?)/.exec(
+          line
+        );
+        if (value) {
+          item.value = value[2].trim();
+          items.push(item);
+          item = {};
+        }
+      }
+    }
+  });
+  extract.discount = items;
 
-    await billingRepo.save(billing);
+  const billingRepo = getManager().getRepository(Billing);
 
-    let message: any = {
-        userId: correlationid,
-        billingUrl: `/api/billings/${billing.id}`,
-        createdDate: billing.createdDate
-    };
+  let billing: any = {
+    userId: correlationid,
+    createdDate: new Date(),
+    data: extract
+  };
 
-    const io = req.app.get('socketio');
-    io.emit('billing.new', message);
-    res.json(message);
+  await billingRepo.save(billing);
+
+  let message: any = {
+    userId: correlationid,
+    billingUrl: `/api/billings/${billing.id}`,
+    createdDate: billing.createdDate
+  };
+
+  const io = req.app.get("socketio");
+  io.emit("billing.new", message);
+  res.json(message);
 });
 
-router.post('/ymobile/:correlationid', ymobile);
-router.post('/au/:correlationid', au);
+router.post("/kddi/:correlationid", async (req: any, res) => {
+  const correlationid = req.params.correlationid;
+
+  const html: string = req.rawBody;
+
+  const extract: any = {};
+
+  // basic usage
+  const buContent = findString(
+    html,
+    "<h1>今月及び前月のデータ通信量</h1>",
+    '<div class="leadSection'
+  );
+  let items: any = [];
+  let item: any;
+  let block: any;
+
+  buContent.split(/\n/).forEach(line => {
+    line = line.trim();
+    if (line.startsWith("<h4>")) {
+      if (block) {
+        if (item) {
+          block.items.push(item);
+          item = undefined;
+        }
+
+        items.push(block);
+      }
+
+      block = {};
+      block.date = findString(line, "<h4>", "</h4>");
+      block.items = [];
+    } else {
+      if (line.startsWith('<div class="cell alignR">')) {
+        let val = findString(line, '<div class="cell alignR">', "</div>");
+        if (item.value) val += "|" + val;
+        else item.value = "";
+
+        item.value += val;
+      } else {
+        let key = findString(line, '<div class="cell item">', "</div>");
+        if (key) {
+          if (item) {
+            block.items.push(item);
+            item = undefined;
+          }
+
+          if (!item) {
+            item = {
+              key: key
+            };
+          }
+        }
+      }
+    }
+  });
+
+  if (item) {
+    block.items.push(item);
+  }
+
+  items.push(block);
+
+  extract.basicUsage = items;
+
+  const billingRepo = getManager().getRepository(Billing);
+
+  let billing: any = {
+    userId: correlationid,
+    createdDate: new Date(),
+    data: extract
+  };
+
+  await billingRepo.save(billing);
+
+  let message: any = {
+    userId: correlationid,
+    billingUrl: `/api/billings/${billing.id}`,
+    createdDate: billing.createdDate
+  };
+
+  const io = req.app.get("socketio");
+  io.emit("billing.new", message);
+  res.json(message);
+});
+
+router.post("/ymobile/:correlationid", ymobile);
+router.post("/au/:correlationid", au);
+router.post("/docomo/:correlationid", docomo);
 
 export const ExtractController: Router = router;
