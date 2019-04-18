ef49c772e37c5aa331dc141c0acb54facf2bef16
NITIPANP <NITIPANP@THESL00592>
2019-04-18 18:46:26 +0700

    fixed AU

--- server/au.extract.ts
+++ server/au.extract.ts
@@ -15,49 +15,68 @@ const au = async (req: any, res) => {
   let items: any = [];
   let item: any = {};
   let block;
-
+  let capturing = false;
   html.split(/\n/).forEach(line => {
     line = line.trim();
-    if (line.startsWith('<p class="detail-title">')) {
-      const title = findString(line, '<p class="detail-title">', '</p>');
-      if (title) {
-        if (seekPriceTable && priceTableMode) {
-          if (block) {
-            if (!items.find(i => i.date == block.date)) items.push(block);
-          }
 
-          block = {};
-          block.date = title;
-          block.items = [];
-
-          seekPriceTable = false;
-          priceTableMode = false;
-        } else {
-          if (!block) {
-            block = {};
-            block.date = title;
-            block.items = [];
-          }
-        }
-
-        if (!seekPriceTable) seekPriceTable = true;
-        else priceTableMode = false;
+    const detailItemMatch = /<div class="detail-item(.*?)">/.exec(line);
+    if (detailItemMatch) {
+      if (detailItemMatch[0].toLowerCase().indexOf('current') !== -1) {
+        capturing = true;
+        seekPriceTable = false;
+        priceTableMode = false;
+      } else {
       }
     }
 
-    if (seekPriceTable) {
-      if (line.startsWith('<table class="price-table"')) priceTableMode = true;
-    }
+    if (capturing) {
+      if (line.startsWith('<p class="detail-title">')) {
+        const title = findString(line, '<p class="detail-title">', '</p>');
+        if (title) {
+          if (seekPriceTable && priceTableMode) {
+            if (block) {
+              if (!items.find(i => i.date == block.date)) {
+                items.push(block);
+                capturing = false;
+                seekPriceTable = false;
+                priceTableMode = false;
+              }
+            }
 
-    if (priceTableMode) {
-      const key = /<th>(.*?)<\/th>/.exec(line);
-      if (key) item.key = key[1].trim();
+            block = {};
+            block.date = title;
+            block.items = [];
 
-      let value = /<td>(.*?)<\/td>/.exec(line);
-      if (value) {
-        item.value = value[1].trim();
-        block.items.push(item);
-        item = {};
+            seekPriceTable = false;
+            priceTableMode = false;
+          } else {
+            if (!block) {
+              block = {};
+              block.date = title;
+              block.items = [];
+            }
+          }
+
+          if (!seekPriceTable) seekPriceTable = true;
+          else priceTableMode = false;
+        }
+      }
+
+      if (seekPriceTable) {
+        if (line.startsWith('<table class="price-table"'))
+          priceTableMode = true;
+      }
+
+      if (priceTableMode) {
+        const key = /<th>(.*?)<\/th>/.exec(line);
+        if (key) item.key = key[1].trim();
+
+        let value = /<td>(.*?)<\/td>/.exec(line);
+        if (value) {
+          item.value = value[1].trim();
+          block.items.push(item);
+          item = {};
+        }
       }
     }
   });
